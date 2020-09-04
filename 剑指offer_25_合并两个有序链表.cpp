c65b1909b9697f0abf96be8d23303bba00db09af
LMJ <741401831@qq.com>
2020-09-04 20:26:36 +0800

    合并两个有序链表

---
+++ 剑指offer_25_合并两个有序链表.cpp
@@ -0,0 +1,38 @@
+//迭代法
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode(1);
+        ListNode* ret = head;
+        while (l1 != NULL && l2 != NULL) {
+            if (l1->val < l2->val) {
+                head->next = l1;
+                l1 = l1->next;
+            } else {
+                head->next = l2;
+                l2 = l2->next;
+            }
+            head = head->next;
+        }
+        head->next = l1 == NULL ? l2 : l1;
+        return ret->next;
+    }
+};
+
+//递归法
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL) return l2;
+		else if (l2 == NULL) return l1;
+		
+        if (l1->val <= l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else { 
+        l2->next = mergeTwoLists(l1, l2->next);
+       return l2;
+       }
+    }
+};
+
